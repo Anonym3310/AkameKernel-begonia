919e7a86a3c1e422ab832d5fbdd7c5e81b99d771
TTTT555 <albukerko@gmail.com>
2020-10-25 18:49:42 +0000

    Add c t s workaround

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -33,6 +33,50 @@ static const struct file_operations cmdline_proc_fops = {
 };
 
 #ifdef CONFIG_PROC_BEGONIA_CMDLINE
+static char *padding = "                ";
+
+static void replace_flag(char *cmd, const char *flag, const char *flag_new)
+{
+	char *start_addr, *end_addr;
+
+	/* Ensure all instances of a flag are replaced */
+	while ((start_addr = strstr(cmd, flag))) {
+		end_addr = strchr(start_addr, ' ');
+		if (end_addr) {
+			if (strlen(flag)<strlen(flag_new)) {
+				// xx yy=a zz
+				//    ^   ^
+				// xx yy=bb zz
+				int length_to_copy = strlen( start_addr + (strlen(flag) ) ) + 1; // +1 to copy trailing '/0'
+				int length_diff = strlen(flag_new)-strlen(flag);
+				memcpy(start_addr+(strlen(flag)+length_diff), start_addr+(strlen(flag)), length_to_copy);
+				memcpy(start_addr+(strlen(flag)), padding, length_diff);
+			}
+			memcpy(start_addr, flag_new, strlen(flag_new));
+		}
+		else
+			*(start_addr - 1) = '\0';
+	}
+}
+
+static void replace_safetynet_flags(char *cmd)
+{
+	// WARNING: be aware that you can't replace shorter string with longer ones in the function called here...
+	replace_flag(cmd, "androidboot.vbmeta.device_state=unlocked",
+			  "androidboot.vbmeta.device_state=locked  ");
+	replace_flag(cmd, "androidboot.enable_dm_verity=0",
+			  "androidboot.enable_dm_verity=1");
+	replace_flag(cmd, "androidboot.secboot=disabled",
+			  "androidboot.secboot=enabled ");
+	replace_flag(cmd, "androidboot.verifiedbootstate=orange",
+			  "androidboot.verifiedbootstate=green ");
+	replace_flag(cmd, "androidboot.veritymode=logging",
+			  "androidboot.veritymode=enforcing");
+	replace_flag(cmd, "androidboot.veritymode=eio",
+			  "androidboot.veritymode=enforcing");
+
+}
+
 static void append_cmdline(char *cmd, const char *flag_val) {
 	strncat(cmd, " ", 2);
 	strncat(cmd, flag_val, strlen(cmd) + 1);
@@ -71,8 +115,12 @@ static int __init proc_cmdline_init(void)
 {
 #ifdef CONFIG_PROC_BEGONIA_CMDLINE
 	strcpy(patched_cmdline, saved_command_line);
-
+	/*
+	 * Replace various flags from command line seen by userspace in order to
+	 * pass SafetyNet CTS check.
+	 */
 	patch_begonia_cmdline(patched_cmdline);
+	replace_safetynet_flags(patched_cmdline);
 #endif
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
